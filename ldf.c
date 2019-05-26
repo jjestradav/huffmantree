45cf2342ff6a752c0453f24431c6cac61d7924cf
joseisaacz <39786721+joseisaacz@users.noreply.github.com>
2019-05-26 10:46:19 -0600

    f in the chat

--- ldf.c
+++ ldf.c
@@ -1,20 +1,29 @@
 #include <stdio.h>
-#include <stdlib.h> 
+#include <stdlib.h>
 #include <unistd.h>
-void cuentafrecuancia(int *tam, char* file, FILE* fp, char*filename);
-int fsize(FILE *fp);
-int fsize(FILE *fp){
-    int prev=ftell(fp);
-    fseek(fp, 0L, SEEK_END);
-    int sz=ftell(fp);
-    fseek(fp,prev,SEEK_SET); //go back to where we were
-    return sz;
-}
-void cuentafreciuencia(int *tam, char* file,FILE* fp, char*filename){
-fp=fopen(filename, "r");
-int size=fsize(fp); 
-for(int i=0; i< size; i++){
-    
-}
+#include <string.h>
+#define MAX 1024
+void cuentafrecuancia(int *tam, char *file, FILE *fp, char *filename);
+long fsize(FILE *fp, char *name);
+long fsize(FILE *fp, char *name)
+{
+    fp = fopen(name, "rb"); //must be binary read to get bytes
 
+    long size = -1;
+    if (fp)
+    {
+        fseek(fp, 0, SEEK_END);
+        size = ftell(fp) + 1;
+        fclose(fp);
+    }
+    return size;
+}
+void cuentafreciuencia(int *tam, char *file, FILE *fp, char *filename)
+{
+
+    long size = fsize(fp, filename);
+    file = (char *)malloc((size + 1) * sizeof(char));
+    fread(file, size, (size_t)1, fp);
+    fclose(fp);
+    printf("%d", file[0]);
 }
--- main.c
+++ main.c
@@ -1,10 +1,35 @@
-#include "ldf.c"
-int main(int argc, char **argv){
-FILE* file;
-char* filename;
-filename = argv[1];
-int val[256];
-char* text;
-cuentafreciuencia(val, text, file, filename);
-return 0; 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+int main()
+{
+    FILE *fp;
+    char *buffer;
+    long size;
+    int val[256] = {0};
+
+    fp = fopen("/home/jose/Documents/Operativos/huffmantree/piolin.jpg", "rb"); // Open the file in binary mode
+    int ch;
+    while((ch=fgetc(fp))!=EOF){
+        ++val[ch];
+    } 
+    fclose(fp);
+   printf("%d",val[255]);
+         // Read in the entire file
+     
+    return 0;
 }
+
+// FILE* file;
+// char* filename;
+// filename = argv[1];
+// int val[256];
+// char* text;
+// cuentafreciuencia(val, text, file, filename);
+
+
+// fseek(fp, 0, SEEK_END);                                                     // Jump to the end of the file
+    // size = ftell(fp);                                                           // Get the current byte offset in the file
+    // rewind(fp);                                                                 // Jump back to the beginning of the file
+    // buffer = (char *)malloc((size+1)*sizeof(char)); // Enough memory for file + \0
+    // fread(&buffer, size, 1, fp);
