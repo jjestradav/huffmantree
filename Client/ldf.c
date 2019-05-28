70d86fd3f42e0efb505b79d3deee74ea7bef5dd9
joseisaacz <39786721+joseisaacz@users.noreply.github.com>
2019-05-27 22:58:48 -0600

    F in the chat

---
+++ Client/ldf.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#define MAX 1024
+void cuentafrecuancia(int *tam, char *file, FILE *fp, char *filename);
+void cuentafreciuencia(int *tam, char *file, FILE *fp, char *filename)
+{
+
+  
+}
+
+
+
+
+
+
+  // long size = fsize(fp, filename);
+    // file = (char *)malloc((size + 1) * sizeof(char));
+    // fread(file, size, (size_t)1, fp);
+    // fclose(fp);
+    // printf("%d", file[0]);
