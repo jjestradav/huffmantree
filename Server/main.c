fcf3d9006bbfdf5cdc979bee150f29472096337e
joseisaacz <39786721+joseisaacz@users.noreply.github.com>
2019-05-29 23:43:09 -0600

    f boiisss

--- Server/main.c
+++ Server/main.c
@@ -1,25 +1,10 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-int main()
+#include "splitfile.c"
+int main(int argc, char **argv)
 {
-    FILE *fp;
-    char *buffer;
-    long size;
-    int val[256] = {0};
-
-    fp = fopen("/home/jose/Documents/Operativos/huffmantree/ezgif.com-gif-to-mp4.mp4", "rb"); // Open the file in binary mode
-    int ch;
-    while((ch=fgetc(fp))!=EOF){
-        ++val[ch];
-    } 
-    fclose(fp);
-    for(int i=0; i<256; i++){
-   printf("%d",val[i]);
-   printf("\n");
-    }
-         // Read in the entire file
-     
+    FILE *file;
+    char *filename;
+    filename = argv[1];
+    split(file,filename,2);
     return 0;
 }
 
@@ -30,9 +15,25 @@ int main()
 // char* text;
 // cuentafreciuencia(val, text, file, filename);
 
-
 // fseek(fp, 0, SEEK_END);                                                     // Jump to the end of the file
-    // size = ftell(fp);                                                           // Get the current byte offset in the file
-    // rewind(fp);                                                                 // Jump back to the beginning of the file
-    // buffer = (char *)malloc((size+1)*sizeof(char)); // Enough memory for file + \0
-    // fread(&buffer, size, 1, fp);
+// size = ftell(fp);                                                           // Get the current byte offset in the file
+// rewind(fp);                                                                 // Jump back to the beginning of the file
+// buffer = (char *)malloc((size+1)*sizeof(char)); // Enough memory for file + \0
+// fread(&buffer, size, 1, fp);
+
+// FILE *fp;
+//     char *buffer;
+//     long size;
+//     int val[256] = {0};
+
+//     fp = fopen("/home/jose/Documents/Operativos/huffmantree/ezgif.com-gif-to-mp4.mp4", "rb"); // Open the file in binary mode
+//     int ch;
+//     while((ch=fgetc(fp))!=EOF){
+//         ++val[ch];
+//     }
+//     fclose(fp);
+//     for(int i=0; i<256; i++){
+//    printf("%d",val[i]);
+//    printf("\n");
+//     }
+//          // Read in the entire file
--- Server/splitfile.c
+++ Server/splitfile.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <math.h>
@@ -6,19 +7,76 @@ void split(FILE *fp, char *filename, int maquinas);
 void split(FILE *fp, char *filename, int maquinas)
 {
     FILE *out;
-    int resultado=0;
-    int maxsize = file_size(filename);
-    int size = sqrt((double)maxsize);
-    if (filename != NULL && maxsize > 0)
+    int max = file_size(filename);
+    int tam = max / maquinas + 1;
+    char buffer[tam];
+    int result;
+    int size;
+    int read;
+    int written;
+    if ((filename != NULL) && (max > 0))
     {
         fp = fopen(filename, "rb");
-        if(fp!=NULL){
-            out=NULL;
-            while(!foef(fp)){
-                if(out==NULL){
+        if (fp != NULL)
+        {
+            out = NULL;
+            result = 1; /* we have at least one part */
 
+            while (!feof(fp))
+            {
+                /* initialize (next) output file if no output file opened */
+                if (out == NULL)
+                {
+                    sprintf(buffer, "%s.%03d", filename, result);
+                    out = fopen(buffer, "wb");
+                    if (out == NULL)
+                    {
+                        result *= -1;
+                        break;
+                    }
+
+                    size = 0;
+                }
+
+                /* calculate size of data to be read from input file in order to not exceed maxSize */
+                read = sizeof(buffer);
+                if ((size + read) > max)
+                {
+                    read = max - size;
+                }
+
+                /* read data from input file */
+                read = fread(buffer, 1, read, fp);
+                if (read == 0)
+                {
+                    result *= -1;
+                    break;
+                }
+
+                /* write data to output file */
+                written = fwrite(buffer, 1, read, out);
+                if (written != read)
+                {
+                    result *= -1;
+                    break;
+                }
+
+                /* update size counter of current output file */
+                size += written;
+                if (size >= max) /* next split? */
+                {
+                    fclose(out);
+                    out = NULL;
+                    result++;
                 }
             }
+
+            /* clean up */
+            if (out != NULL)
+            {
+                fclose(out);
+            }
+            fclose(fp);
         }
     }
 }
@@ -35,3 +93,82 @@ int file_size(char *filename)
     }
     return size;
 }
+
+// int splitFile(char *fileIn, size_t maxSize)
+// {
+//     int result = 0;
+//     FILE *fIn;
+//     FILE *fOut;
+//     char buffer[1024 * 16];
+//     size_t size;
+//     size_t read;
+//     size_t written;
+
+//     if ((fileIn != NULL) && (maxSize > 0))
+//     {
+//         fIn = fopen(fileIn, "rb");
+//         if (fIn != NULL)
+//         {
+//             fOut = NULL;
+//             result = 1;   /* we have at least one part */
+
+//             while (!feof(fIn))
+//             {
+//                 /* initialize (next) output file if no output file opened */
+//                 if (fOut == NULL)
+//                 {
+//                     sprintf(buffer, "%s.%03d", fileIn, result);
+//                     fOut = fopen(buffer, "wb");
+//                     if (fOut == NULL)
+//                     {
+//                         result *= -1;
+//                         break;
+//                     }
+
+//                     size = 0;
+//                 }
+
+//                 /* calculate size of data to be read from input file in order to not exceed maxSize */
+//                 read = sizeof(buffer);
+//                 if ((size + read) > maxSize)
+//                 {
+//                     read = maxSize - size;
+//                 }
+
+//                 /* read data from input file */
+//                 read = fread(buffer, 1, read, fIn);
+//                 if (read == 0)
+//                 {
+//                     result *= -1;
+//                     break;
+//                 }
+
+//                 /* write data to output file */
+//                 written = fwrite(buffer, 1, read, fOut);
+//                 if (written != read)
+//                 {
+//                     result *= -1;
+//                     break;
+//                 }
+
+//                 /* update size counter of current output file */
+//                 size += written;
+//                 if (size >= maxSize)   /* next split? */
+//                 {
+//                     fclose(fOut);
+//                     fOut = NULL;
+//                     result++;
+//                 }
+//             }
+
+//             /* clean up */
+//             if (fOut != NULL)
+//             {
+//                 fclose(fOut);
+//             }
+//             fclose(fIn);
+//         }
+//     }
+
+//     return (result);
+// }
