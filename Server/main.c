5c1752df10e1268aa3cb1fb96dea3e4b07a5a1ca
joseisaacz <39786721+joseisaacz@users.noreply.github.com>
2019-05-31 00:36:03 -0600

    fcite

--- Server/main.c
+++ Server/main.c
@@ -12,19 +12,10 @@ void *splitf(void *p)
     strcpy(src, cant);
     strcpy(dest, filename);
     strcat(src, dest);
-    // printf("%s", src);
-    // printf("\n");
-    
-    char dest1[100];    
+    char dest1[100];
     strcpy(dest1, "split -n ");
     strcat(dest1, src);
-    // printf("%s", dest1);
-    //  printf("\n");
-     char *argv[10];
-     argv[0]=dest1;
-    printf("%s", argv[0]);
-    printf("\n");
-  system(dest1);
+    system(dest1);
 }
 pthread_t splitfile;
 int main(int argc, char **argv)
@@ -36,39 +27,6 @@ int main(int argc, char **argv)
     pthread_join(splitfile, NULL);
     printf("termino la otra vara pero el programa no. AMEN");
     printf("\n");
-    printf("%d", 5+5+5+5);
-    printf("\n");
-    // split(file,filename,5);
-
+    
     return 0;
 }
-
-// FILE* file;
-// char* filename;
-// filename = argv[1];
-// int val[256];
-// char* text;
-// cuentafreciuencia(val, text, file, filename);
-
-// fseek(fp, 0, SEEK_END);                                                     // Jump to the end of the file
-// size = ftell(fp);                                                           // Get the current byte offset in the file
-// rewind(fp);                                                                 // Jump back to the beginning of the file
-// buffer = (char *)malloc((size+1)*sizeof(char)); // Enough memory for file + \0
-// fread(&buffer, size, 1, fp);
-
-// FILE *fp;
-//     char *buffer;
-//     long size;
-//     int val[256] = {0};
-
-//     fp = fopen("/home/jose/Documents/Operativos/huffmantree/ezgif.com-gif-to-mp4.mp4", "rb"); // Open the file in binary mode
-//     int ch;
-//     while((ch=fgetc(fp))!=EOF){
-//         ++val[ch];
-//     }
-//     fclose(fp);
-//     for(int i=0; i<256; i++){
-//    printf("%d",val[i]);
-//    printf("\n");
-//     }
-//          // Read in the entire file
